d82644bf54726713e55b1a2ed01f64f1240ae94d
magilo <53962625+magilo@users.noreply.github.com>
2022-01-18 03:17:02 -0500

    connection needs to use ssl

--- server/db/db.js
+++ server/db/db.js
@@ -3,12 +3,30 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
+let config
+
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+} else {
+  config = {
     logging: false
   }
+}
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
 )
+
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
@@ -16,3 +34,22 @@ module.exports = db
 if (process.env.NODE_ENV === 'test') {
   after('close database connection', () => db.close())
 }
+
+// const Sequelize = require('sequelize')
+// const pkg = require('../../package.json')
+
+// const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+//   {
+//     logging: false
+//   }
+// )
+// module.exports = db
+
+// // This is a global Mocha hook used for resource cleanup.
+// // Otherwise, Mocha v4+ does not exit after tests.
+// if (process.env.NODE_ENV === 'test') {
+//   after('close database connection', () => db.close())
+// }
